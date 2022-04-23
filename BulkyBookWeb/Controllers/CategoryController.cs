d569d339dcb37d9f8d9166a960379b922fcf2ae0
HadiAfzalizadeh <hadiafzalizadeh@gmail.com>
2022-04-23 19:47:39 +0430

    1401/02/03   7:47

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -26,5 +26,103 @@ namespace BulkyBookWeb.Controllers
             return View();
         }
 
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+            var categorFromDb = _db.Categories.Find(id);
+            var categorFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            var categorFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categorFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorFromDb);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+            var categorFromDb = _db.Categories.Find(id);
+            var categorFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            var categorFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (categorFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorFromDb);
+        }
+
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
     }
 }
