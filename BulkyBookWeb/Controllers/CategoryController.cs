e5fcf6edb4a18e531afca4b6327b33ae1d3fab71
HadiAfzalizadeh <hadiafzalizadeh@gmail.com>
2022-04-23 12:37:59 +0430

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContxt _db;
+
+        public CategoryController(ApplicationDbContxt db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContxt.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContxt : DbContext
+    {
+        
+
+        public ApplicationDbContxt(DbContextOptions<ApplicationDbContxt> options) : base(options)
+        {
+             
+        }
+
+        public DbSet<Category> Categories { get; set; }  
+
+    }
+}
